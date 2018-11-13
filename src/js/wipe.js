34d3ebfaa3423f7e93fb6d017cf7d791cea83338
JX0108 <2896468203@qq.com>
2018-11-13 10:07:59 +0800

    增加移动端支持

--- src/js/wipe.js
+++ src/js/wipe.js
@@ -1,10 +1,15 @@
 var cas = document.getElementById('cas');
 var context =cas.getContext("2d");
 var _w = cas.width,_h=cas.height;
-var raduis = 30 //涂抹的半径
+var raduis = 10 //涂抹的半径
 var movex,movey;
 var isMouseDown=false;
 var t = 0
+var device = (/android|webos|iPhone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+// console.log(device);
+var Press = device ? 'touchstart':'mousedown';
+var Move = device ? 'touchmove':'mousemove';
+var Uplift  = device ? 'touchend':'mouseup';
 
 
 function drawMask(context){
@@ -22,50 +27,32 @@ function drawPoint(context,movex,movey){
 	context.restore();
 }
 function drawLine(context,x1,y1,x2,y2){
+	console.log("传递参数的个数"+arguments.length)
 	context.save();
+	context.beginPath();
 	context.lineCap="round"
 	context.lineWidth = raduis*2;
-	context.beginPath();
 	context.moveTo(x1,y1);
 	context.lineTo(x2,y2);
 	context.stroke();
 	context.restore();
 }
-// 在canvas画布上监听自定义事件，mousedown，调用drawPoint图形
-cas.onclick=function(){
-
-}
-cas.addEventListener('mousedown',function(event){
-		var evt = event || window.event;
-		// 获取鼠标在视口的坐标，传递参数到drawpoint的
-		 movex= evt.clientX;
-		 movey= evt.clientY;
-		 drawPoint(context,movex,movey);
-		 isMouseDown = true;
-},false);
-
-cas.addEventListener('mousemove',function(event){
-	if (isMouseDown){
-		var evt = event || window.event;
-		// 获取鼠标在视口的坐标，传递参数到drawpoint的
-		var mox= evt.clientX;
-		var moy= evt.clientY;
-		// drawPoint(context,movex,movey);
-		drawLine(context,movex,movey,mox,moy);
-		// 把每次结束点变成下一次划线的开始点
-		movex = mox;
-	    movey = moy;
-	}else{
-		return false
-	}
-},false);
-cas.addEventListener('mouseup',function(event){
-	isMouseDown = false;
-	if ( getTransparencyPercent(context)>50) {
-		alert("超过了50%的面积");
-		clearRect(context);
-	};
-},false);
+// function drawRound(context,x1,y1,x2,y2){
+// 	context.save();
+// 	context.beginPath();
+// 	if (arguments.length==3) {
+// 		context.arc(x1,x2,raduis,0,2*Math.PI);
+// 		context.fillStyle="red";
+// 		context.fill();
+// 	}else{
+// 		context.lineCap="round"
+// 		context.lineWidth = raduis*2;
+// 		context.moveTo(x1,y1);
+// 		context.lineTo(x2,y2);
+// 		context.stroke();
+// 	}
+// 	context.restore();
+// }
 function clearRect(context){
 	context.clearRect(0,0,_w,_h);
 }
@@ -86,3 +73,45 @@ function getTransparencyPercent(context){
 window.onload=function(){
 	drawMask(context);
 }
+
+// 在canvas画布上监听自定义事件，mousedown，调用drawPoint图形
+// cas.addEventListener('mousedown',down,false);
+cas.addEventListener(Press,down,false);
+function down(event){
+		var evt = event || window.event;
+		// 获取鼠标在视口的坐标，传递参数到drawpoint的
+ 		movex= device ? evt.touches[0].clientX:evt.clientX;
+		movey= device ? evt.touches[0].clientY:evt.clientY;
+		 drawPoint(context,movex,movey);
+		 isMouseDown = true;
+}
+
+// cas.addEventListener('mousemove',move,false);
+cas.addEventListener(Move,move,false);
+function move(event){
+	if (isMouseDown){
+		var evt = event || window.event;
+		evt.preventDefault(); 
+		// 获取鼠标在视口的坐标，传递参数到drawpoint的
+		var mox= device ? evt.touches[0].clientX:evt.clientX;
+		var moy= device ? evt.touches[0].clientY: evt.clientY;
+		// drawPoint(context,movex,movey);
+		drawLine(context,movex,movey,mox,moy);
+		// 把每次结束点变成下一次划线的开始点
+		movex = mox;
+	    movey = moy;
+	}else{
+		return false
+	}
+}
+// cas.addEventListener('mouseup',fn2,false);
+cas.addEventListener(Uplift,function(event){
+	t=0
+	isMouseDown = false;
+	if ( getTransparencyPercent(context)>50) {
+		alert("超过了50%的面积");
+		clearRect(context);
+	};
+},false);
+
+
